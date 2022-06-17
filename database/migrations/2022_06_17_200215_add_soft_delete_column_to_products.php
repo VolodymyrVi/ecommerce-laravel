d6e04895e6584b3cba1dab200f9bbb89a4325a22
VolodymyrVi <sqlphpjs@gmail.com>
2022-06-17 23:08:25 +0300

    fix

--- database/migrations/2022_06_17_200215_add_soft_delete_column_to_products.php
+++ database/migrations/2022_06_17_200215_add_soft_delete_column_to_products.php
@@ -14,7 +14,7 @@ class AddSoftDeleteColumnToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->softDeletes;
+            $table->softDeletes();
         });
     }
 
@@ -26,7 +26,7 @@ class AddSoftDeleteColumnToProducts extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->softDeletes;
+            $table->softDeletes();
         });
     }
 }
