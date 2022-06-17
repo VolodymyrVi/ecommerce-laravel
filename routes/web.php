a60d6a0b8c13af7ddfe1f04f2120d22fd52b3fd0
VolodymyrVi <sqlphpjs@gmail.com>
2022-06-17 18:19:21 +0300

    added routing for category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\Admin\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', [IndexController::class, 'index'])->name('admin.main.index');
+
+
+Route::group(['prefix' => 'admin/category'], function(){
+    Route::get('/', [CategoryController::class, 'index'])->name('admin.category.index');
+    Route::get('create', [CategoryController::class, 'create'])->name('admin.category.create');
+    Route::post('/', [CategoryController::class, 'store'])->name('admin.category.store');
+    Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('admin.category.edit');
+    Route::get('/{category}', [CategoryController::class, 'show'])->name('admin.category.show');
+    Route::patch('/{category}', [CategoryController::class, 'update'])->name('admin.category.update');
+    Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('admin.category.delete');
+});
+
