507853c980662f9abc223405bd0f85afa598709f
VolodymyrVi <sqlphpjs@gmail.com>
2022-06-19 16:49:26 +0300

    added  home page route, controller and view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\Admin\IndexController;
+use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,9 +17,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('frontend.main.index');
+
 
 Route::get('/admin', [IndexController::class, 'index'])->name('admin.main.index');
 
