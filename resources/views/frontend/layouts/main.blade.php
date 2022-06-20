73a953c00fad12d9f5f3a148ea97d8d20f23caa7
VolodymyrVi <sqlphpjs@gmail.com>
2022-06-20 17:23:30 +0300

    changed templates

--- resources/views/frontend/layouts/main.blade.php
+++ resources/views/frontend/layouts/main.blade.php
@@ -328,67 +328,7 @@
                 </div>
             </div>
         </div>
-        <div class="sidebar-content-closer"></div>
-        <div class="sidebar-content">
-            <div class="sidebar-widget-container">
-                <div class="widget-heading d-flex justify-content-end align-content-center"> <span
-                        class="close-side-widget">X</span> </div>
-                <div class="sidebar-textwidget">
-                    <div class="sidebar-info-contents">
-                        <div class="content-inner">
-                            <div class="logo"> <a href="index.html"><img
-                                        src="{{ asset('frontend/images/logo/logo-2.png') }}" alt=""></a>
-                            </div>
-                            <div class="content-box">
-                                <h4>About Us</h4>
-                                <div class="inner-text">
-                                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
-                                        Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
-                                </div>
-                            </div>
-                            <div class="form_inner">
-                                <h4>Support</h4>
-                                <form action="index.html" method="post">
-                                    <div class="form-group mt-4"> <input type="text" name="name"
-                                            placeholder="Name" required=""> </div>
-                                    <div class="form-group mt-4"> <input type="email" name="email"
-                                            placeholder="Email" required=""> </div>
-                                    <div class="form-group mt-4">
-                                        <textarea name="message" placeholder="Message..."></textarea>
-                                    </div>
-                                    <div class="form-group message-btn mt-4"> <button type="submit"
-                                            class="btn--secondary"> <span class="txt">Submit Now</span> </button>
-                                    </div>
-                                </form>
-                            </div>
-                            <div class="sidebar-contact-info">
-                                <h4>Contact Info</h4>
-                                <ul>
-                                    <li> <span class="flaticon-pin-1"></span> New York, United States </li>
-                                    <li> <span class="flaticon-telephone"></span> <a href="tel:+44203700001">+44 123
-                                            456
-                                            789</a> </li>
-                                    <li> <span class="flaticon-mail"></span> <a
-                                            href="mailto:info@example.com">info@example.com</a> </li>
-                                </ul>
-                            </div>
-                            <div class="thm-medio-boxx1">
-                                <ul class="social-box">
-                                    <li class="facebook"> <a href="https://www.facebook.com/" target="_blank"><i
-                                                class="flaticon-facebook-app-symbol"></i></a> </li>
-                                    <li class="twitter"> <a href="https://twitter.com/" target="_blank"><i
-                                                class="flaticon-twitter"></i></a> </li>
-                                    <li class="instagram"> <a href="https://www.instagram.com/" target="_blank"><i
-                                                class="flaticon-instagram"></i></a> </li>
-                                    <li class="youtube"> <a href="https://www.youtube.com/" target="_blank"><i
-                                                class="flaticon-youtube"></i></a> </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
 
     </header>
 
@@ -407,73 +347,63 @@
                         <div class="footer-default__single-box">
                             <div class="company-info">
                                 <div class="footer-title">
-                                    <h4> Office</h4>
+                                    <h4> Офіс</h4>
                                 </div>
                                 <div class="text1">
-                                    <p>29 Holles Place, Dublin 2 D02 YY46</p>
+                                    <p>м.Київ</p>
                                 </div>
                                 <div class="text2">
-                                    <p>68 Jay Street, Suite 902 New Side <br> Brooklyn, NY 11201</p>
+                                    <p>м.Коростишів</p>
                                 </div>
-                                <div class="text3">
-                                    <p>New York, United States</p>
-                                </div>
+
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-2 col-lg-6 col-md-6 col-sm-12 mt-30 wow fadeInUp animated">
                         <div class="footer-default__single-box">
                             <div class="footer-title">
-                                <h4> Useful Links </h4>
+                                <h4> Корисні посиланні </h4>
                             </div>
                             <ul class="footer-links">
-                                <li><a href="my-account.html">Account</a></li>
-                                <li><a href="login.html">Sign In</a></li>
-                                <li><a href="cart.html">View Cart</a></li>
-                                <li><a href="wishlist.html">My WishList</a></li>
-                                <li><a href="compare.html">Compare Products</a></li>
+                                <li><a href="my-account.html">Акаунт</a></li>
+                                <li><a href="login.html">Увійти</a></li>
+                                <li><a href="cart.html">Переглянути корзину</a></li>
+                                <li><a href="wishlist.html">Мій список бажань</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 mt-30 wow fadeInUp animated">
                         <div class="footer-default__single-box">
                             <div class="footer-title">
-                                <h4> Information </h4>
+                                <h4> Інформація </h4>
                             </div>
                             <ul class="footer-links">
-                                <li><a href="about-us.html">About us</a></li>
-                                <li><a href="contact.html">Contact Us </a></li>
-                                <li><a href="faq.html">Faq</a></li>
-                                <li><a href="blog.html">Latest Posts</a></li>
-                                <li><a href="order-track.html">Order Track</a></li>
+                                <li><a href="about-us.html">Оплата і достаставка</a></li>
+                                <li><a href="contact.html">Контакти </a></li>
+                                <li><a href="faq.html">Відповіді на часті питання</a></li>
+                                <li><a href="blog.html">Остінні станні</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 mt-30 wow fadeInUp animated">
                         <div class="footer-default__single-box">
                             <div class="footer-title">
-                                <h4> Newsletter </h4>
+                                <h4> Підписка на новини </h4>
                             </div>
                             <div class="footer-newsletter">
-                                <p class="text">Enter your email to receive our latest updates about our products &
-                                    promotions. </p>
+                                <p class="text">Введіть ваш e-mail, щоб отримувати останні новини і пропозиції від нашої компанії. </p>
                                 <form action="#0" class="footer-default__subscrib-form">
-                                    <div class="footer-input-box"> <input type="email" placeholder="Email address"
-                                            name="email"> <button type="submit" class="subscribe_btn"> Subscribe
+                                    <div class="footer-input-box"> <input type="email" placeholder="Email адреса"
+                                            name="email"> <button type="submit" class="subscribe_btn"> Підписатись
                                         </button> </div>
                                 </form>
                                 <div class="newsletter-bottom d-flex align-items-center">
                                     <div class="footer-title-box">
-                                        <p>Follow Us:</p>
+                                        <p>Слідкуй за нами:</p>
                                     </div>
                                     <div class="footer__medio-boxx  medio-boxx  d-flex align-items-center">
                                         <ul>
-                                            <li><a href="https://www.facebook.com/" target="_blank" class="active"><i
-                                                        class="flaticon-facebook-app-symbol"></i></a></li>
-                                            <li><a href="https://www.youtube.com/" target="_blank"><i
-                                                        class="flaticon-youtube"></i></a></li>
-                                            <li><a href="https://twitter.com/"><i class="flaticon-twitter"
-                                                        target="_blank"></i></a></li>
+                                            
                                             <li><a href="https://www.instagram.com/"><i class="flaticon-instagram"
                                                         target="_blank"></i></a></li>
                                         </ul>
